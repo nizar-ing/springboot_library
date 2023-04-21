0da8616f5a95d6cd836a83f89a2ed4c420e0d8fd
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-21 20:15:35 +0100

    Creating the History entity in order to create a relationship between it and the history table for further use.

---
+++ src/main/java/com/nizaring/springbootlibrary/entity/History.java
@@ -0,0 +1,49 @@
+package com.nizaring.springbootlibrary.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="history")
+@Data  @NoArgsConstructor
+public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="user_email")
+    private String userEmail;
+
+    @Column(name="checkout_date")
+    private String checkoutDate;
+
+    @Column(name="returned_date")
+    private String returnedDate;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="author")
+    private String author;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="img")
+    private String img;
+
+    public History(String userEmail, String checkoutDate, String returnedDate, String title, String author, String description, String img) {
+        this.userEmail = userEmail;
+        this.checkoutDate = checkoutDate;
+        this.returnedDate = returnedDate;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.img = img;
+    }
+}
