f492ccb73f50ad7c4721f6c8a37b005be3158374
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-22 15:18:30 +0100

    implementing our Message Controller

---
+++ src/main/java/com/nizaring/springbootlibrary/controller/MessagesController.java
@@ -0,0 +1,24 @@
+package com.nizaring.springbootlibrary.controller;
+
+import com.nizaring.springbootlibrary.entity.Message;
+import com.nizaring.springbootlibrary.service.MessagesService;
+import com.nizaring.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+    private MessagesService messagesService;
+
+    @Autowired
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    public void postMessage(@RequestHeader(value = "Authorization") String token, @RequestBody Message requestMessage){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messagesService.postMessage(requestMessage, userEmail);
+    }
+}
