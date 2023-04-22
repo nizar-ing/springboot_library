a0a7f96ee47ed0a6e6a846aa16c9acf4b5f442e1
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-22 22:46:46 +0100

    Creating our open ticket endpoint

--- src/main/java/com/nizaring/springbootlibrary/dao/MessageRepository.java
+++ src/main/java/com/nizaring/springbootlibrary/dao/MessageRepository.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
+    Page<Message> findByClosed(@RequestParam("closed") boolean closed, Pageable pageable);
 }
