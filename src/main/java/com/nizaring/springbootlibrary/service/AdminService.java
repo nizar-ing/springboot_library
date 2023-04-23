ca6c57bc023a8112a0818d207195fbdded80a7f1
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-23 20:16:10 +0100

    Creating our Admin Service layer

---
+++ src/main/java/com/nizaring/springbootlibrary/service/AdminService.java
@@ -0,0 +1,32 @@
+package com.nizaring.springbootlibrary.service;
+
+import com.nizaring.springbootlibrary.dao.BookRepository;
+import com.nizaring.springbootlibrary.entity.Book;
+import com.nizaring.springbootlibrary.requestmodels.AddBookRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AdminService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public AdminService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void postBook(AddBookRequest addBookRequest){
+        var book = new Book();
+        book.setTitle(addBookRequest.getTitle());
+        book.setAuthor(addBookRequest.getAuthor());
+        book.setDescription(addBookRequest.getDescription());
+        book.setCopies(addBookRequest.getCopies());
+        book.setCopiesAvailable(addBookRequest.getCopies());
+        book.setCategory(addBookRequest.getCategory());
+        book.setImg(addBookRequest.getImage());
+        bookRepository.save(book);
+    }
+
+}
