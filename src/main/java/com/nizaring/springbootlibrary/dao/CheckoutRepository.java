d5f14d69323e57786c0b7ea2aa81d1bf59ed596c
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-24 00:25:55 +0100

    Enhancing repositories for delete

--- src/main/java/com/nizaring/springbootlibrary/dao/CheckoutRepository.java
+++ src/main/java/com/nizaring/springbootlibrary/dao/CheckoutRepository.java
@@ -2,6 +2,9 @@ package com.nizaring.springbootlibrary.dao;
 
 import com.nizaring.springbootlibrary.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +12,8 @@ public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
 
     List<Checkout> findBooksByUserEmail(String userEmail);
+
+    @Modifying
+    @Query("delete from Checkout where book_id in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
--- src/main/java/com/nizaring/springbootlibrary/dao/ReviewRepository.java
+++ src/main/java/com/nizaring/springbootlibrary/dao/ReviewRepository.java
@@ -4,10 +4,17 @@ import com.nizaring.springbootlibrary.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 
     Review findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    @Modifying
+    @Query("delete from Review where bookId in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long book_id);
 }
