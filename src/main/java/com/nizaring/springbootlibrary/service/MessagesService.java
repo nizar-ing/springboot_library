99d9d21f126448ea36621f18717239b616466054
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-23 16:23:00 +0100

    Enhancing our messages Service to update the user message by adding an admin response.

--- src/main/java/com/nizaring/springbootlibrary/service/MessagesService.java
+++ src/main/java/com/nizaring/springbootlibrary/service/MessagesService.java
@@ -2,9 +2,12 @@ package com.nizaring.springbootlibrary.service;
 
 import com.nizaring.springbootlibrary.dao.MessageRepository;
 import com.nizaring.springbootlibrary.entity.Message;
+import com.nizaring.springbootlibrary.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -20,5 +23,14 @@ public class MessagesService {
         var message = new Message(requestMessage.getTitle(), requestMessage.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String adminEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+        if(!message.isPresent()) throw new Exception("message not found");
+        message.get().setAdminEmail(adminEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 }
