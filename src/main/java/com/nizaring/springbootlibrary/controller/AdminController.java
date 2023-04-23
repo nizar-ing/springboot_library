8ba3e841b98d31315682febe7dd4f8356e4a9c22
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-24 00:07:21 +0100

    Decrease Book quantity Controller

--- src/main/java/com/nizaring/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/nizaring/springbootlibrary/controller/AdminController.java
@@ -24,6 +24,13 @@ public class AdminController {
         adminService.increaseBookQuantity(bookId);
     }
 
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token, @RequestParam("bookId") Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")) throw new Exception("Administration role only");
+        adminService.decreaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token, @RequestBody AddBookRequest addBookRequest) throws Exception {
         String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
