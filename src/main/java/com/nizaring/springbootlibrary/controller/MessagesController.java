92314d4421cb371fcf3b3ffcd8a6254cbaaeb307
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-04-23 16:37:11 +0100

    Enhancing our put messages Controller endpoint

--- src/main/java/com/nizaring/springbootlibrary/controller/MessagesController.java
+++ src/main/java/com/nizaring/springbootlibrary/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.nizaring.springbootlibrary.controller;
 
 import com.nizaring.springbootlibrary.entity.Message;
+import com.nizaring.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.nizaring.springbootlibrary.service.MessagesService;
 import com.nizaring.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class MessagesController {
     public void postMessage(@RequestHeader(value = "Authorization") String token, @RequestBody Message requestMessage){
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(requestMessage, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token, @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")) throw new Exception("Administration role only");
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
